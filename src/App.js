117a077c240da0e4d87f77c4ed06d4c15c28e333
JSpru <jgspruance@gmail.com>
2017-04-24 17:00:36 -0700

    Update App.js
    Removing test objects

--- src/App.js
+++ src/App.js
@@ -17,46 +17,6 @@ import './App.css';
 import JSONPretty from 'react-json-pretty';
 
 
-var testObjF = {
-  "firstName": "John",
-  "lastName": "Green",
-  "car": {
-    "make": "Honda",
-    "model": "Civic",
-    "revisions": [
-      { "miles": 10150, "code": "REV01", "changes": "test" },
-      { "miles": 20021, "code": "REV02", "changes": [
-        { "type": "asthetic", "desc": "Left tire cap" },
-        { "type": "mechanic", "desc": "Engine pressure regulator" }
-      ] }
-    ]
-  },
-  "visits": [
-    { "date": "2015-01-01", "dealer": "DEAL-001" },
-    { "date": "2015-03-01", "dealer": "DEAL-002" }
-  ]
-};
-
-var testObjUf = {
-    "firstName": "John",
-    "lastName": "Green",
-    "car.make": "Honda",
-    "car.model": "Civic",
-    "car.revisions.0.miles": 10150,
-    "car.revisions.0.code": "REV01",
-    "car.revisions.0.changes": "ttt",
-    "car.revisions.1.miles": 20021,
-    "car.revisions.1.code": "REV02",
-    "car.revisions.1.changes.0.type": "asthetic",
-    "car.revisions.1.changes.0.desc": "Left tire cap",
-    "car.revisions.1.changes.1.type": "mechanic",
-    "car.revisions.1.changes.1.desc": "Engine pressure regulator",
-    "visits.0.date": "2015-01-01",
-    "visits.0.dealer": "DEAL-001",
-    "visits.1.date": "2015-03-01",
-    "visits.1.dealer": "DEAL-002"
-}
-
 // create a placeholder for the formatted output objects
 var outputFlatObj = {},
     outputUnFlatObj = {};
